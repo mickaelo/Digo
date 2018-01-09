cba77301eb1101974553bf46b5ce3399c081a4b2
Mick <mickael.ottmann@viacesi.fr>
2018-01-09 12:01:58 +0100

    root

--- application/views/head.php
+++ application/views/head.php
@@ -6,5 +6,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
     <meta charset="utf-8">
     <title>Welcome to CodeIgniter</title>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Candal|Alegreya+Sans">
+    <link rel="stylesheet" type="text/css" href="./assets/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/imagehover.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
 </head>
+
+<!--Navigation bar-->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">Men<span>tor</span></a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#feature">Features</a></li>
+                <li><a href="#organisations">Organisations</a></li>
+                <li><a href="#courses">Courses</a></li>
+                <li><a href="#pricing">Pricing</a></li>
+                <li><a href="#" data-target="#login" data-toggle="modal">Sign in</a></li>
+                <li class="btn-trial"><a href="#footer">Free Trail</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
